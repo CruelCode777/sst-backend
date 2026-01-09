6f490067012f506094320097b0a95659fcccb542
CruelCode777 <carlosaajr@gmail.com>
2026-01-09 10:42:19 -0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -50,7 +50,7 @@ with col2:
                     # ATENÇÃO: Verifique no seu /docs qual o nome exato do endpoint
                     # Vou assumir que é /gerar_relatorio, mas pode ser outro.
                     # ---------------------------------------------------------
-                    response = requests.post(f"{API_URL}/gerar_pdf", json=dados)
+                    response = requests.post(f"{API_URL}/api/gerar_relatorio", json=dados)
                     
                     if response.status_code == 200:
                         st.success("Relatório Gerado com Sucesso!")
@@ -68,4 +68,5 @@ with col2:
                 except Exception as e:
 
                     st.error(f"Erro de conexão: {e}")
+
 
