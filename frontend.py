1e973afa7b4bcaf0c0a804241b6ff713db743f2a
CruelCode777 <carlosaajr@gmail.com>
2026-01-09 11:51:50 -0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -3,7 +3,7 @@ import requests
 
 # --- CONFIGURAÇÃO ---
 # ⚠️ CONFIRA SE ESTE É O SEU LINK DO RENDER (SEM BARRA NO FINAL)
-API_URL = https://sst-backend.onrender.com
+API_URL = https://https://sst-ai-suite.onrender.com
 st.set_page_config(page_title="SST.AI Suite", page_icon="🛡️", layout="wide")
 
 # --- ESTILO CYBERPUNK (CSS) ---
@@ -191,5 +191,6 @@ with col2:
                     
             except Exception as e:
                 st.error(f"Erro Crítico: {e}")
+
 
 
